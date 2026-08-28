c61685f700591cebc1c276cf7c86ed0295cb2030
kavyavenkatesh-10027 <kavyavenkatesh2007@gmail.com>
2026-08-28 17:57:13 +0530

    String template code snippet

--- src/beginner/helloWorld/StringTemplate.kt
+++ src/beginner/helloWorld/StringTemplate.kt
@@ -1,1 +1,10 @@
 package beginner.helloWorld
+
+fun main() {
+    val customers = 10
+    val entryTicketPrice = 100
+
+    println("There are $customers customers")
+
+    println("The money earned from entry tickets is Rs. ${customers*entryTicketPrice}")
+}
